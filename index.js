0c38177488f64013614f2b3e304deafa6645bcdb
Avinashluhana <aviluhana@gmail.com>
2021-08-12 13:48:18 +0500

    portfolio with express

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require("express");
-const { copyFileSync } = require("fs");
 const path = require("path");
+const hbs = require("hbs");
+
 const app = express();
-console.log(path.join(__dirname,"index.html"))
+console.log(path.join(__dirname,"/view/partials"))
+
+
+app.set("view engine", "hbs");
+
 
 // reading the html file through express.js
 
-const staticPath = path.join(__dirname)
-
+const staticPath = path.join(__dirname);
+const partialPath = path.join(__dirname, "/views/partials");
 app.use(express.static(staticPath));
+
+hbs.registerPartials(partialPath);
 
 app.get("/", (req,res) => {
     // res.send("Express is working");
+    res.render("index");
+
 })
 
 app.get("/about", (req, res) =>{
---
+++ views/index.js
@@ -0,0 +1,18 @@
+// const express = require ("express");
+
+
+// const app = express();
+
+// console.log(__dirname);
+
+// app.set("view engine", "hbs");
+
+// app.get("/",(req,res) =>{
+//     res.render("templateEngine");
+// })
+
+
+
+// app.listen(6000, () =>{
+//     console.log("rendering data");
+// })
